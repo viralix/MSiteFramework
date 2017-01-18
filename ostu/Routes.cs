19e470c3715b31c0ca21801a7a5b682ed0c1e46c
viralix <viralstark@gmail.com>
2017-01-18 17:42:55 +0100

    From Viral

---
+++ ostu/Routes.cs
@@ -0,0 +1,46 @@
+﻿using SimpleHttpServer.Models;
+using System.Collections.Generic;
+namespace ostu
+{
+	public class Routes
+	{
+		public List<Route> routes
+		{
+			get
+			{
+				return new List<Route>()
+				{
+					new Route()
+					{
+						Callable = Handle,
+						UrlRegex = "(\\/)",
+						Method = "GET"
+					},
+					new Route()
+					{
+						Callable = Handle,
+						UrlRegex = "(\\/)",
+						Method = "POST"
+					}
+				};
+
+			}
+		}
+		public List<Route> Handler()
+		{
+			return routes;
+		}
+
+		public HttpResponse Handle(HttpRequest request)
+		{
+			
+			return new HttpResponse()
+			{
+				ContentAsUTF8 = "<h1>Zdravo, Svetu!</h1>",
+				// Your own response
+			};
+		}
+	}
+
+}
+
